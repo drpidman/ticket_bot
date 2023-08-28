d23fb637429f22d3e1d857998eb919ef897fe4a8
drpidman <augustocaio663@gmail.com>
2023-08-28 01:29:23 -0300

    melhorar legibilidade na verificação

--- src/interactions/ticket_menu.rs
+++ src/interactions/ticket_menu.rs
@@ -34,14 +34,11 @@ pub async fn ticket_menu(ctx: &Context, component: &MessageComponentInteraction,
     let tickets_channel = ChannelId::from(TICKET_LOG_CHANNEL.parse::<u64>().unwrap());
     let tickets_category = ChannelId::from(TICKET_CREATION_CATEGORY.parse::<u64>().unwrap());
 
-    if TicketConfig::get(component.guild_id.unwrap().0)
-        .unwrap()
-        .unwrap()
-        .ticket_id
-        != component.message.id.0
-    {
-        return;
-    }
+    if let Some(ticket) = TicketConfig::get(component.guild_id.unwrap().0).unwrap() {
+        if ticket.ticket_id != component.message.id.0 {
+            return;
+        }
+    };
 
     let mut ticket_embed = CreateEmbed::default();
     let mut ticket_channel = CreateChannel::default();
