fde6a07622bc794f6b5e69040d95c456bb475ebe
drpidman <augustocaio663@gmail.com>
2023-08-29 00:46:25 -0300

    Manipulação de evento correta para ButtonInteraction

--- src/interactions/ticket_button_actions.rs
+++ src/interactions/ticket_button_actions.rs
@@ -1,6 +1,9 @@
 use serenity::{prelude::Context, model::prelude::message_component::MessageComponentInteraction};
 
+use crate::utils::components::ticket::channel_parser;
+
 
 pub async fn ticket_button_action(ctx: &Context, component: &MessageComponentInteraction) {
     
+    component.channel_id.delete(&ctx).await.unwrap();
 }
